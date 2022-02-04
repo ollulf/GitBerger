632cb8711c192d22aa8023762396a2bda2df392e
LucasBoe <lucas@lumotex.de>
2022-02-04 16:14:25 +0100

    Added some words from Miro

--- Assets/Scripts/CommitMessageTextComponent.cs
+++ Assets/Scripts/CommitMessageTextComponent.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CommitMessageTextComponent : ScriptableObject
 {
-    public string Text => name;
+    public string Text => name == "Custom" ? custom : name;
     public CommitMessageTextComponent[] TextArray => GetTextArray();
     public CommitMessageTextComponent[] possibleFollowups;
+
+    public string custom = "";
     protected virtual CommitMessageTextComponent[] GetTextArray()
     {
         return new CommitMessageTextComponent[] { this };
